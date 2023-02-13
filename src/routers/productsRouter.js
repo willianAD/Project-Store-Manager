de60e6c289d05ebd75b33cd89d7ea3913b71d1b0
willianAD <willian.afonso013@gmail.com>
2023-02-13 18:07:14 -0300

    Requisito 4

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { productsController } = require('../controllers');
 
+const validateNameProducts = require('../middlewares/validateNameProducts');
+
 const router = express.Router();
 
 router.get('/', productsController.productsAll);
 
 router.get('/:id', productsController.productsId);
 
-router.post('/', productsController.insertProducts);
+router.post('/', validateNameProducts, productsController.insertProducts);
 
 module.exports = router;
